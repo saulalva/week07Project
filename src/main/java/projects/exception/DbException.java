52303032ac7e3427175c81c2ecd0591218aa7846
Saul Alvarez <overtex@Mac.attlocal.net>
2025-03-30 15:20:24 -0700

    second commit

---
+++ src/main/java/projects/exception/DbException.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package projects.exception;
+
+/**
+ * 
+ */
+public class DbException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+
+
+	/**
+	 * @param message
+	 */
+	public DbException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public DbException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public DbException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	
+	
+
+}
